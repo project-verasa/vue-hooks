0d72d3da99c9d8934c3184e22e513748ab216a01
gxmari007 <392041299@qq.com>
2020-06-24 00:54:36 +0800

    update code

--- src/use-store/interface.ts
+++ src/use-store/interface.ts
@@ -1,11 +1,32 @@
 import { Ref } from '@vue/composition-api';
 
-type Dictionary<T> = { [key: string]: T };
-type Computed = Ref<any>;
+type Computed<T = any> = Ref<T>;
+type CustomVue = Vue & Record<string, any>;
+type InlineComputed<T extends Function> = T extends (...args: any[]) => infer R ? Computed<R> : never;
 
 interface Mapper<R> {
-  (map: string[]): Dictionary<R>;
-  (map: Dictionary<string>): Dictionary<R>;
+  <Key extends string>(map: Key[]): { [K in Key]: R };
+  <Map extends Record<string, string>>(map: Map): { [K in keyof Map]: R };
 }
 
-export type UseState = Mapper<Computed>;
+interface MapperWithNamespace<R> {
+  <Key extends string>(namespace: string, map: Key[]): { [K in Key]: R };
+  <Map extends Record<string, string>>(namespace: string, map: Map): {
+    [K in keyof Map]: R;
+  };
+}
+
+interface MapperForState {
+  <S, Map extends Record<string, (this: CustomVue, state: S, getters: any) => any> = {}>(map: Map): {
+    [K in keyof Map]: InlineComputed<Map[K]>;
+  };
+}
+
+interface MapperForStateWithNamespace {
+  <S, Map extends Record<string, (this: CustomVue, state: S, getters: any) => any> = {}>(
+    namespace: string,
+    map: Map
+  ): {};
+}
+
+export type UseState = Mapper<Computed> & MapperWithNamespace<Computed> & MapperForState & MapperForStateWithNamespace;
--- src/use-store/vuex.ts
+++ src/use-store/vuex.ts
@@ -1,13 +1,39 @@
 import { mapState } from 'vuex';
+import { computed, Ref } from '@vue/composition-api';
+
+import { UseState } from './interface';
+import { getRuntimeVM } from '../utils/runtime';
+
+type Computed<T = any> = Ref<T>;
+type CustomVue = Vue & Record<string, any>;
+type InlineComputed<T extends Function> = T extends (...args: any[]) => infer R ? Computed<R> : never;
 
 export enum Helper {
   State,
 }
 
-const helpers = {
-  [Helper.State]: { fn: mapState }
-};
-
-export function createVuexHelper(h: Helper) {
-  const helper = helpers[h];
+function handleComputed(mappedFn: Function) {
+  return computed(() => mappedFn.call(getRuntimeVM()));
 }
+
+// export function useState<Key extends string>(map: Key[]): { [K in Key]: Computed };
+// export function useState<Map extends Record<string, string>>(map: Map): { [K in keyof Map]: Computed };
+// export function useState<Key extends string>(namespace: string, map: Key[]): { [K in Key]: Computed };
+// export function useState<Map extends Record<string, string>>(
+//   namespace: string,
+//   map: Map
+// ): {
+//   [K in keyof Map]: Computed;
+// };
+// export function useState<S, Map extends Record<string, (this: CustomVue, state: S, getters: any) => any> = {}>(
+//   map: Map
+// ): {
+//   [K in keyof Map]: InlineComputed<Map[K]>;
+// };
+// export function useState<S, Map extends Record<string, (this: CustomVue, state: S, getters: any) => any> = {}>(
+//   namespace: string,
+//   map: Map
+// ): {};
+// export function useState(map: unknown): any {}
+
+export const useState: UseState = (namespace?: string, map: unknown): any => {};
