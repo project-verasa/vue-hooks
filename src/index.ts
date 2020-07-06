0eb392a4254698548bc57d2f6b7f5773637c8278
gxmari007 <392041299@qq.com>
2020-07-06 16:48:49 +0800

    feat(src): 添加 useDebounce、useDebounceFn

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import '@vue/composition-api';
 
 import { setRuntimeVM } from './utils/runtime';
 
+export * from './use-debounce';
 export * from './use-media';
 export * from './use-router';
 export * from './use-store';
---
+++ src/use-debounce/use-debounce.ts
@@ -0,0 +1,18 @@
+import { Ref, ref, watch } from '@vue/composition-api';
+
+import { useDebounceFn } from './use-debounce-fn';
+
+export function useDebounce<T>(value: Ref<T>, delay: number = 250): Ref<T> {
+  if (delay <= 0) {
+    return value;
+  }
+
+  const delayValue: Ref<T> = ref(value.value) as Ref<T>;
+  const debounce = useDebounceFn(() => {
+    delayValue.value = value.value;
+  }, delay);
+
+  watch(value, debounce);
+
+  return delayValue;
+}
